c3834671b14d501ab818327eff9d0bf4f7ee742e
Huhani <mmia268@gmail.com>
2021-04-28 01:14:10 +0000

    Revert "불필요한 정보 삭제"
    This reverts commit 2f50d6fc2d141fed1d97efd60a5d3c069e87a7fa.

--- modules/noti/server/config.js
+++ modules/noti/server/config.js
@@ -5,7 +5,7 @@ module.exports = {
         port: 5672,
         vhost: "noti_vhost",
         userID: "noti_push",
-        userPW: "noti_password",
+        userPW: "nOt1t3stAcc0unt",
         retry: 10, // sec
         triggerPrefetchCount: 5,
         pushPrefetchCount: 120
@@ -13,19 +13,19 @@ module.exports = {
     RhymixDB: {
         host: "127.0.0.1",
         port: 3306,
-        dbName: "",
-        dbUserID: "rx_",
-        dbUserPW: "",
+        dbName: "rx_test",
+        dbUserID: "rx_test",
+        dbUserPW: "rhymix_test!@",
         prefix: "rx_",
         connectionLimit: 120
     },
     VAPID: {
-        publicKey: "",
-        privateKey: "",
-        subject: ""
+        publicKey: "BIXKW3zzWsF5957dyS-vYPvg_vU8i8rL7nkDu6uj5SLPM7ERllkKrj6dUfNirA86UqTo6NJQTVXstv6C2BMXtVs",
+        privateKey: "oh0Mnq5muwS3xNb8SS8ETsbijaLYOZn_ZZ27dpGGElc",
+        subject: "mailto:mmia268@gmail.com"
     },
     FCM: {
-        serverKey: "",
-        id: ""
+        serverKey: "AAAAFqYj14I:APA91bEIP4znqrJQInF3vVVg3VQSaQEkB1kMqJ5JCEH1tLv5vJtGlplcvyC_s3XOQBIRG8aQ8ZzPDgIgQK0YfrMCvC4o57HPsKW90ZKk691-9IUHKsKBXDnOTNj0jZCPVCH-CQ3gWief",
+        id: "97276647298"
     }
 };
