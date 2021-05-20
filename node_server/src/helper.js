86eaaa9ff10a13dd7cfcfeda117ab303b06bc821
Huhani <mmia268@gmail.com>
2021-05-20 08:18:09 +0000

    node파일 정리

--- node_server/src/helper.js
+++ node_server/src/helper.js
@@ -1,13 +1,6 @@
+var moment = require('moment-timezone');
 var getDate = function() {
-    var date_ob = new Date();
-    var date = ("0" + date_ob.getDate()).slice(-2);
-    var month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    var year = date_ob.getFullYear();
-    var h = ("0" + date_ob.getHours()).slice(-2);
-    var m = ("0" + date_ob.getMinutes()).slice(-2);
-    var s = ("0" + date_ob.getSeconds()).slice(-2);
-
-    return year + month + date+ h + m + s;
+    return moment().tz('Asia/Seoul').format('YYYYMMDDHHmmss');
 };
 
 module.exports = {
